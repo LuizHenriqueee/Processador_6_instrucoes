1c91fa16fe0c8c45d34433af8cf7eb6587b23298
KaioVCarvalho <103061389+KaioVCarvalho@users.noreply.github.com>
2022-04-07 19:10:02 -0300

    Update bloco_controle.vhd

--- bloco_controle.vhd
+++ bloco_controle.vhd
@@ -24,6 +24,10 @@ architecture synth of bloco_controle is -- declaração da variavel estados e si
 	
 
 begin
+	-- sinais que serão utilizados pela maquina de estados para transferir os dados esperados para cada saída de controle 
+	-- op deve comandar a qual operação a ser realizada pelo bloco operacional
+	-- r, ra, rb, rc são registradores utilizados para carregar os valores nas operações de soma, subtração entre outras
+	-- d, o e c são posições de memória.
     op <= IR(15 downto 12);
 	r <= IR(11 downto 8);
 	ra <= IR(11 downto 8);
@@ -33,7 +37,7 @@ begin
 	c <= IR(7 downto 0);
 	o <= IR(7 downto 0);
 
-process (clk) begin -- registrador de estados
+process (clk) begin -- registrador de estados que promove a mudança de estado sincrona ao sinal de clk, respeitando o reset que retorna a máquina para estado "inicio"
 		if clk'event and clk = '1' then
 			if reset = '1' then state <= inicio;
 			else state <= nextstate;
@@ -45,7 +49,7 @@ process (clk) begin -- registrador de estados
 		case state is
 			when inicio => nextstate <= busca;
 			when busca => nextstate <= decodifica;
-			when decodifica => case op is
+			when decodifica => case op is -- novo case determina qual operação comparando op com os valores binários de 0 a 5, correspondentes a 6 intruções
 						when "0000" => nextstate <= carrega;
 						when "0001" => nextstate <= armazena;
 						when "0010" => nextstate <= soma;
@@ -55,7 +59,7 @@ process (clk) begin -- registrador de estados
 						when others => nextstate <= inicio; -- caso não esperado
 					   end case;
 		
-			when saltar_se => case RF_Rp_zero is
+			when saltar_se => case RF_Rp_zero is --identificando se a flag0 está ativa para que o estado saltar seja lido ou não
 						when '1' => nextstate <= saltar;
 						when '0' => nextstate <= busca;
 						when others => nextstate <= inicio; -- caso não esperado
@@ -73,7 +77,7 @@ process (clk) begin -- registrador de estados
 	end process;
 	
 	process (state) begin --atualizaçãoo das saídas para cada estado
-	-- reset de todas as saídas para manipular apenas as desejadas
+			      -- reset de todas as saídas para manipular apenas as desejadas
 			PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
 			I_rd <= '0';IR_id <= '0';
 			D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
@@ -81,7 +85,7 @@ process (clk) begin -- registrador de estados
 			RF_W_addr <= "0000"; RF_W_data <= "00000000"; RF_W_wr <= '0'; RF_s0 <= '0';
 			RF_s1 <= '0'; alu_s0 <= '0'; alu_s1 <= '0';
 	
-	case state is
+	case state is -- case de definição, onde a partir do estado as variaveis de controle são alteradas para comandar a operação dada pela intrução atual.
 			when inicio => PC_clr <= '1';
 			when busca => I_rd <= '1';
 				PC_inc <= '1';
