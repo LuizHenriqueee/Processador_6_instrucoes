0df373333a7e967791955417a778d0d0d7676d44
KaioVCarvalho <103061389+KaioVCarvalho@users.noreply.github.com>
2022-04-07 20:00:57 -0300

    finalizado e comentários verificados

--- Bloco operacional/flag0.vhd
+++ Bloco operacional/flag0.vhd
@@ -3,14 +3,11 @@ use IEEE.STD_LOGIC_1164.all;
 
 entity flag0 is
 	port(	a: in STD_LOGIC_VECTOR(15 downto 0);
-			y: out STD_LOGIC);
+		y: out STD_LOGIC);
 end flag0;
 
 architecture synth of flag0 is
-	signal i: integer;
-	signal x: STD_LOGIC_VECTOR(15 downto 1);
-begin -- N-bit AND of inverted inputs
-
+begin 
 	-- Verifica se o valor de a é igual a 0x0000
 	with a select
 	y <= '1' when "0000000000000000",
