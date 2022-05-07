fec472c73790d21ff127d365c062e28a4ea629b2
unknown <luizhenrique.nunesdeoliveira@gmail.com>
2022-05-06 21:37:26 -0300

    Mundaças feitas pelo Luiz

--- Processador completo/bloco_controle.vhd
+++ Processador completo/bloco_controle.vhd
@@ -10,21 +10,21 @@ entity bloco_controle is -- entradas e saídas do bloco de controle
 				PC_id, PC_clr, PC_inc, I_rd, IR_id: out STD_LOGIC;
 				RF_W_wr, RF_Rp_rd, RF_Rq_rd: out STD_LOGIC;
 				alu_s1, alu_s0: out STD_LOGIC;
-				RF_W_addr, RF_Rp_addr, 
+				RF_W_addr, RF_Rp_addr,
 				RF_Rq_addr: out STD_LOGIC_VECTOR(3 downto 0));
 end bloco_controle;
 
 architecture synth of bloco_controle is -- declaração da variavel estados e sinais internos
-	
+
 	type statetype is (inicio, busca, decodifica, carrega, armazena, soma,
 	constante, subtrair, saltar_se, verifica, saltar);
 	signal state, nextstate: statetype;
 	signal op, r, ra, rb, rc:STD_LOGIC_VECTOR(3 downto 0);
 	signal d, c, o: STD_LOGIC_VECTOR(7 downto 0);
-	
+
 
 begin
-	-- sinais que serão utilizados pela maquina de estados para transferir os dados esperados para cada saída de controle 
+	-- sinais que serão utilizados pela maquina de estados para transferir os dados esperados para cada saída de controle
 	-- op deve comandar a qual operação a ser realizada pelo bloco operacional
 	-- r, ra, rb, rc são registradores utilizados para carregar os valores nas operações de soma, subtração entre outras
 	-- d, o e c são posições de memória.
@@ -44,12 +44,38 @@ process (clk) begin -- registrador de estados que promove a mudança de estado s
 			end if;
 		end if;
 	end process;
-	
+
 	process (state, op) begin -- logica de mudança de estados
 		case state is
-			when inicio => nextstate <= busca;
-			when busca => nextstate <= decodifica;
-			when decodifica => case op is -- novo case determina qual operação comparando op com os valores binários de 0 a 5, correspondentes a 6 intruções
+			when inicio =>
+			PC_clr <= '1';
+			PC_id <= '0'; PC_inc <= '0';
+			I_rd <= '0';IR_id <= '0';
+			D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
+			RF_Rp_addr <= "0000"; RF_Rp_rd <= '0'; RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
+			RF_W_addr <= "0000"; RF_W_data <= "00000000"; RF_W_wr <= '0'; RF_s0 <= '0';
+			RF_s1 <= '0'; alu_s0 <= '0'; alu_s1 <= '0';
+			nextstate <= busca;
+			when busca =>
+			I_rd <= '1';
+				PC_inc <= '1';
+				IR_id <= '1';
+			PC_clr <= '0'; PC_id <= '0';
+			D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
+			RF_Rp_addr <= "0000"; RF_Rp_rd <= '0'; RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
+			RF_W_addr <= "0000"; RF_W_data <= "00000000"; RF_W_wr <= '0'; 
+			RF_s0 <= '0';RF_s1 <= '0'; alu_s0 <= '0'; alu_s1 <= '0'; 
+			nextstate <= decodifica;
+			
+			when decodifica => 
+			PC_clr <= '0';
+			PC_id <= '0'; PC_inc <= '0';
+			I_rd <= '0';IR_id <= '0';
+			D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
+			RF_Rp_addr <= "0000"; RF_Rp_rd <= '0'; RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
+			RF_W_addr <= "0000"; RF_W_data <= "00000000"; RF_W_wr <= '0'; RF_s0 <= '0';
+			RF_s1 <= '0'; alu_s0 <= '0'; alu_s1 <= '0';
+					case op is -- novo case determina qual operação comparando op com os valores binários de 0 a 5, correspondentes a 6 intruções
 						when "0000" => nextstate <= carrega;
 						when "0001" => nextstate <= armazena;
 						when "0010" => nextstate <= soma;
@@ -58,166 +84,123 @@ process (clk) begin -- registrador de estados que promove a mudança de estado s
 						when "0101" => nextstate <= saltar_se;
 						when others => nextstate <= inicio; -- caso não esperado
 					   end case;
-		
-			when saltar_se => nextstate <= verifica;
-			
+
+			when saltar_se =>
+			 RF_Rp_addr <= ra;
+			    RF_Rp_rd <= '1';
+			PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
+			I_rd <= '0';IR_id <= '0';
+			D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
+			RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
+			RF_W_addr <= "0000"; RF_W_data <= "00000000"; RF_W_wr <= '0'; 
+			RF_s0 <= '0';RF_s1 <= '0'; alu_s0 <= '0'; alu_s1 <= '0';			
+			nextstate <= verifica;
+
 			when verifica =>
 			case RF_Rp_zero is --identificando se a flag0 está ativa para que o estado saltar seja lido ou não
 						when '1' => nextstate <= saltar;
-						--when '0' => nextstate <= busca;
-						when others => nextstate <= busca; 
+						when '0' => nextstate <= busca;
+						when others => nextstate <= inicio; -- caso não esperado
 					  end case;
-	
-			when carrega => nextstate <= busca;
-			when armazena => nextstate <= busca;
-			when soma => nextstate <= busca;
-			when constante => nextstate <= busca;
-			when subtrair => nextstate <= busca;
-			when saltar => nextstate <= busca;
-			when others => nextstate <= inicio; -- caso não esperado
-		end case;
-	
-	end process;
-	
-	process (state) begin --atualizaçãoo das saídas para cada estado
-	-- reset de todas as saídas para manipular apenas as desejadas
-			-- PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
-			-- I_rd <= '0';IR_id <= '0';
-			-- D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
-			-- RF_Rp_addr <= "0000"; RF_Rp_rd <= '0'; RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
-			-- RF_W_addr <= "0000"; RF_W_data <= "00000000"; RF_W_wr <= '0'; RF_s0 <= '0';
-			-- RF_s1 <= '0'; alu_s0 <= '0'; alu_s1 <= '0';
-	
-	case state is -- case de definição, onde a partir do estado as variaveis de controle são alteradas para comandar a operação dada pela intrução atual.
-			when inicio => PC_clr <= '1';
-			
-			PC_id <= '0'; PC_inc <= '0';
-			I_rd <= '0';IR_id <= '0';
-			D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
-			RF_Rp_addr <= "0000"; RF_Rp_rd <= '0'; RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
-			RF_W_addr <= "0000"; RF_W_data <= "00000000"; RF_W_wr <= '0'; RF_s0 <= '0';
-			RF_s1 <= '0'; alu_s0 <= '0'; alu_s1 <= '0';
-			
-			when busca => I_rd <= '1';
-				PC_inc <= '1';
-				IR_id <= '1';
-				
-			PC_clr <= '0'; PC_id <= '0'; 
-			D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
-			RF_Rp_addr <= "0000"; RF_Rp_rd <= '0'; RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
-			RF_W_addr <= "0000"; RF_W_data <= "00000000"; RF_W_wr <= '0'; RF_s0 <= '0';
-			RF_s1 <= '0'; alu_s0 <= '0'; alu_s1 <= '0';
-			
-			when carrega => D_addr <= d;
+
+			when carrega => 
+			D_addr <= d;
 				D_rd <= '1';
 				RF_s1 <= '0';
 				RF_s0 <= '1';
 				RF_W_addr <= ra;
 				RF_W_wr <='1';
-				
-				PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
+			PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
 			I_rd <= '0';IR_id <= '0';
-			 D_wr <= '0';
+			D_wr <= '0';
 			RF_Rp_addr <= "0000"; RF_Rp_rd <= '0'; RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
-			 RF_W_data <= "00000000"; 
-			 alu_s0 <= '0'; alu_s1 <= '0';
-			
-			when armazena => D_addr <= d;
+			RF_W_data <= "00000000";
+			alu_s0 <= '0'; alu_s1 <= '0';			
+			nextstate <= busca;
+
+			when armazena =>
+			D_addr <= d;
 				RF_Rp_addr <= ra;
 				RF_Rp_rd <='1';
 				D_wr <= '1';
 				RF_s1 <= 'X';
 				RF_s0 <= 'X';
-
-PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
+			PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
 			I_rd <= '0';IR_id <= '0';
-			 D_rd <= '0'; 
-			  RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
+			D_rd <= '0';
+			RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
 			RF_W_addr <= "0000"; RF_W_data <= "00000000"; RF_W_wr <= '0'; 
-			 alu_s0 <= '0'; alu_s1 <= '0';
-			
-			when soma => RF_Rp_addr <= rb;
+			alu_s0 <= '0'; alu_s1 <= '0';			
+			nextstate <= busca;
+
+			when soma =>
+			RF_Rp_addr <= rb;
+				RF_Rp_addr <= rb;
 				RF_Rp_rd <= '1';
+
+				RF_Rq_addr <= rc;
+				RF_Rq_rd <= '1';
+
+				RF_W_addr <= ra;
 				RF_s1 <= '0';
 				RF_s0 <= '0';
-				RF_Rq_addr <= rc;
-				
-				RF_Rq_rd <= '1';
-				RF_W_addr <= ra;							
-				
 				alu_s0 <= '1';
 				alu_s1 <= '0';
-				RF_W_wr <='1';	
-
-PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
+				RF_W_wr <='1';
+			PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
 			I_rd <= '0';IR_id <= '0';
 			D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
-			RF_Rp_addr <= "0000";   
-			 RF_W_data <= "00000000";  
-			 
-			
-			when constante => RF_s1 <= '1';
+			RF_W_data <= "00000000"; 
+			nextstate <= busca;
+
+			when constante =>
+			RF_s1 <= '1';
 				RF_s0 <= '0';
 				RF_W_addr <= ra;
 				RF_W_data <= c;
 				RF_W_wr <= '1';
-				
-				PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
+			PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
 			I_rd <= '0';IR_id <= '0';
 			D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
-			RF_Rp_addr <= "0000"; RF_Rp_rd <= '0'; RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
-			 alu_s0 <= '0'; alu_s1 <= '0';
-			
-			when subtrair => RF_Rp_addr <= rb;
+			RF_Rp_addr <= "0000"; RF_Rp_rd <= '0'; RF_Rq_addr <= "0000"; RF_Rq_rd <= '0'; 
+			alu_s0 <= '0'; alu_s1 <= '0';
+			nextstate <= busca;
+
+			when subtrair =>
+			RF_Rp_addr <= rb;
+				RF_Rp_addr <= rb;
 				RF_Rp_rd <= '1';
+
 				RF_s1 <= '0';
 				RF_s0 <= '0';
+
 				RF_Rq_addr <= rc;
 				RF_Rq_rd <= '1';
+				
 				RF_W_addr <= ra;
 				RF_W_wr <='1';
 				alu_s0 <= '0';
 				alu_s1 <= '1';
-				
-				PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
+			PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
 			I_rd <= '0';IR_id <= '0';
 			D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
-			 
-			 RF_W_data <= "00000000"; 
-			
-			when saltar_se => 
-			    RF_Rp_addr <= ra;
-				RF_Rp_rd <= '1';
-				
-				PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
-			I_rd <= '0';IR_id <= '0';
-			D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
-			 RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
-			RF_W_addr <= "0000"; RF_W_data <= "00000000"; RF_W_wr <= '0'; RF_s0 <= '0';
-			RF_s1 <= '0'; alu_s0 <= '0'; alu_s1 <= '0';
-			
-			when verifica => 
-			    RF_Rp_addr <= ra;
-				RF_Rp_rd <= '1';
-				
-				PC_clr <= '0'; PC_id <= '0'; PC_inc <= '0';
-			I_rd <= '0';IR_id <= '0';
-			D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
-			 RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
-			RF_W_addr <= "0000"; RF_W_data <= "00000000"; RF_W_wr <= '0'; RF_s0 <= '0';
-			RF_s1 <= '0'; alu_s0 <= '0'; alu_s1 <= '0';
-			when saltar => 
-			    PC_id <= '1';
-				
-				PC_clr <= '0'; PC_inc <= '0';
+			RF_W_data <= "00000000";			
+			nextstate <= busca;
+
+			when saltar =>
+			PC_id <= '1';
+			PC_clr <= '0'; PC_inc <= '0';
 			I_rd <= '0';IR_id <= '0';
 			D_addr <= "00000000"; D_rd <= '0'; D_wr <= '0';
 			RF_Rp_addr <= "0000"; RF_Rp_rd <= '0'; RF_Rq_addr <= "0000"; RF_Rq_rd <= '0';
-			RF_W_addr <= "0000"; RF_W_data <= "00000000"; RF_W_wr <= '0'; RF_s0 <= '0';
-			RF_s1 <= '0'; alu_s0 <= '0'; alu_s1 <= '0';
+			RF_W_addr <= "0000"; RF_W_data <= "00000000"; RF_W_wr <= '0'; 
+			RF_s0 <= '0';RF_s1 <= '0'; alu_s0 <= '0'; alu_s1 <= '0';			
+			nextstate <= busca;
 			
-			when others => null;
-end case;
-end process;
+			when others => nextstate <= inicio; -- caso não esperado
+		end case;
+
+	end process;
+
 end;
 
